4c9b2c2354569bab45aaa6ea245fd3482b33ee63
Luke Renchik <lukerenchik@gmail.com>
2024-03-10 10:21:47 -0400

    with the sprites this time

--- scripts/npc_20.gd
+++ scripts/npc_20.gd
@@ -1,28 +1,11 @@
 extends CharacterBody2D
 
+func _ready():
+	# Start idle animation when NPC is initialized
+	$AnimatedSprite2D.play("idle_front")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _on_Player_interact():
+	# Pause idle animation when interacting with the NPC
+	$AnimatedSprite2D.stop()
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	$AnimatedSprite2D.play("idle_front")
